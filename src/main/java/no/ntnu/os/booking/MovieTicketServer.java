f3aa5aa127c6184307717e23d9f9d98940410ef7
Harald <haraldwf@stud.ntnu.no>
2023-02-21 12:18:13 +0100

    Removed import not used

--- src/main/java/no/ntnu/os/booking/MovieTicketServer.java
+++ src/main/java/no/ntnu/os/booking/MovieTicketServer.java
@@ -1,9 +1,5 @@
 package no.ntnu.os.booking;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class MovieTicketServer {
   // TBD
   private String movieName;
